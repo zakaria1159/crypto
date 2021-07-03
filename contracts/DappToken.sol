dc06953c782c9419e2ed15ddea003e34a2c8934a
Zakaria <z.boumarouane@gmail.com>
2021-07-03 18:35:33 +0100

    2, transfer tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,17 +2,31 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract DappToken {
     //name
-    string  public name = "DApp Token";
-    string  public symbol= "DAPP";
-    string  public standard= "Dapp Token v1.0";
+    string public name = "DApp Token";
+    string public symbol = "DAPP";
+    string public standard = "Dapp Token v1.0";
     uint256 public totalSupply;
-    
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
 
-    constructor (uint _initialSupply) public {
+    constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
 
-   // function transfer(address _to, uint256 _value) 
+    function transfer(address _to, uint256 _value)public returns (bool success){
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
 }
